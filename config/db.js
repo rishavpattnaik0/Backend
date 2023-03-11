3c8384c7033ddc0ae925ef3c60369042d780fdf8
rishav <rishavpattnaik0@gmail.com>
2023-03-12 00:22:05 +0530

    Changes

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://rishavpattnaik0:20169361@cluster0.nvx61ne.mongodb.net/?retryWrites=true&w=majority"
+// console.log(MONGO_URL)
+// Connect Database
+export const connectDB = mongoose.connect(MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(console.log("DB Connected Succesfully...."))
+.catch((err)=>{
+    console.log("DB Connection Failed!")
+    console.log(err)
+    process.exit(1)
+});
+
+export default connectDB;
