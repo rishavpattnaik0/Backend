57c52d46feca54f6e200b12e1840f87b4b6f5529
rishav <rishavpattnaik0@gmail.com>
2023-03-12 00:40:43 +0530

    Changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ import User from "../models/User.js"
 
 // Making Promise
 import bigPromise from "../middlewares/bigPromise.js"
-import { cookieToken } from "../utils/Cookietoken";
+import { cookieToken } from "../utils/Cookietoken.js";
 export const createUser = bigPromise(async(req,res,next)=>{
     const {username,email,password}=req.body;
     console.log(req.body);
